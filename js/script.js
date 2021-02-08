de9a917b09f825ee90fc8f1534c919870b7b1b34
Antor Biswas <you@example.combiswasantor20@gmail.com>
2021-02-08 21:59:55 +0600

    Complete Hungry Monoster Project

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ const displayMeal = meals => {
         const mealsDiv = document.createElement('div');
         mealsDiv.className = 'col-md-3';
         mealsDiv.innerHTML = `
-            <div onclick="foodIngredient()"; class="border bg-white text-center" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <div onclick="foodIngredient('${food.idMeal}')"; class="border bg-white text-center" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 <img class="img-fluid" src="${food.strMealThumb}" alt="" />
                 <h2 class="h4 p-2">${food.strMeal}</h2>
             </div>
@@ -24,13 +24,11 @@ const displayMeal = meals => {
     });
 };
 
-const foodIngredient = () => {
-    const url =` https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772`
+const foodIngredient = (id) => {
+    const url =` https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            //console.log(data);
-            //displayMeal(data.meals);
             ingredientInfo(data.meals);
         });
 };
@@ -39,10 +37,15 @@ const ingredientInfo = ingredient => {
     const infoDiv = document.getElementById('ingredientDetails');
     ingredient.forEach(element => {
         infoDiv.innerHTML = `
-        <img class="img-fluid" src="${element.strMealThumb}" alt="" />
-        <h2 class="h4 p-2">${element.strIngredient1}</h2>
+        <img class="img-fluid mb-3" src="${element.strMealThumb}" alt="" />
+        <li>${element.strIngredient1}</li>
+        <li>${element.strIngredient2}</li>
+        <li>${element.strIngredient3}</li>
+        <li>${element.strIngredient4}</li>
+        <li>${element.strIngredient5}</li>
+        <li>${element.strIngredient6}</li>
+        <li>${element.strIngredient7}</li>
+        <li>${element.strIngredient8}</li>
         `;
-
-        console.log(element);
     });
 };
